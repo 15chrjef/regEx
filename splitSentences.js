01b3691c3ca3676d166b9b3babc5fa4256c73b9d
Jeffrey Emerich Christian <15chrjef@gmail.com>
2017-02-04 16:24:27 -0800

    create splitSentences.js

---
+++ splitSentences.js
@@ -0,0 +1,40 @@
+`Edit the Expression & Text to see matches. Roll over matches or the expression for details. Undo mistakes with cmd-z. Save Favorites & Share expressions with friends or the Community. Explore your results with Tools. A full Reference & Help is available in the Library, or watch the video Tutorial.
+Sample text for testing:
+abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ
+0123456789 _+-.,!@#$%^&*();\/|<>"'
+12345 -98.7 3.141 .6180 9,000 +42
+555.123.4567	+1-(800)-555-2468
+foo@demo.net	bar.ba@test.co.uk
+www.demo.com	http://foo.co.uk/
+http://regexr.com/foo.html?q=bar
+https://mediatemple.net`.split(/[!|\.|\?]+/g)
+
+/*
+ [ 'Edit the Expression & Text to see matches',
+  ' Roll over matches or the expression for details',
+  ' Undo mistakes with cmd-z',
+  ' Save Favorites & Share expressions with friends or the Community',
+  ' Explore your results with Tools',
+  ' A full Reference & Help is available in the Library, or watch the video Tutorial',
+  '\nSample text for testing:\nabcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ\n0123456789 _+-',
+  ',',
+  '@#$%^&*();/',
+  '<>"\'\n12345 -98',
+  '7 3',
+  '141 ',
+  '6180 9,000 +42\n555',
+  '123',
+  '4567\t+1-(800)-555-2468\nfoo@demo',
+  'net\tbar',
+  'ba@test',
+  'co',
+  'uk\nwww',
+  'demo',
+  'com\thttp://foo',
+  'co',
+  'uk/\nhttp://regexr',
+  'com/foo',
+  'html',
+  'q=bar\nhttps://mediatemple',
+  'net' ]
+*/
